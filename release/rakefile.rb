3eb7269e2814f9f3574daddc6a16b1f3faaed397
Mike O'Brien <mob@mikeobrien.net>
2010-08-08 21:56:07 -0400

    Added an ignored folder

--- release/rakefile.rb
+++ release/rakefile.rb
@@ -6,7 +6,7 @@ desc "Deploys the application."
 robocopy :deploy do |rc|
     rc.source = "src"
     rc.target = "D:/Websites/www.clintobrien.org/wwwroot"
-    rc.excludeDirs = "obj"
+    rc.excludeDirs = "obj GuestBook"
     rc.includeFiles = "*.dll *.pdb *.config *.asax *.ascx *.ashx " \
                       "*.aspx *.master *.htm *.html *.txt *.css " \
                       "*.gif *.jpg *.jpeg *.png *.xml *.js"
