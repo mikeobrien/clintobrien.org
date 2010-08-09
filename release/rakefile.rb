bdd8a1f6e13a165dfd339bc501933f75fb8c01c1
Mike O'Brien <mob@mikeobrien.net>
2010-08-08 21:05:34 -0400

    Added build script

---
+++ release/rakefile.rb
@@ -0,0 +1,14 @@
+require "release/robocopy"
+
+task :default => :deploy
+
+desc "Deploys the application."
+robocopy :deploy do |rc|
+    rc.source = "src"
+    rc.target = "D:/Websites/www.clintobrien.org/wwwroot"
+    rc.excludeDirs = "obj"
+    rc.includeFiles = "*.dll *.pdb *.config *.asax *.ascx *.ashx " \
+                      "*.aspx *.master *.htm *.html *.txt *.css " \
+                      "*.gif *.jpg *.jpeg *.png *.xml *.js"
+    rc.logPath = "Robocopy.log"
+end
